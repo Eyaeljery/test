580013043841492a8d88061d3351fda6b0254a20
eya jery <jaryeya2000@gmail.com>
2023-02-23 11:59:18 +0100

    commit

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from github import Github
 import pandas as pd
 
 project_list=['Eyaeljery/projet2','Eyaeljery/projet ']
-token="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"
+token="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"
 def project_info():
-    proects=pd.DataFrame()
+    df_projects=pd.DataFrame()
     for projects in project_list:
         g=Github(token)
+        repo=g.get_repo(projects)
+        df_projects=df_projects.append({'project_id':repo.id,
+                                        'name':repo.full_name })
+        print(df_projects)                 
 
 
 
