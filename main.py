387f33e6ece52c6daf7be3291c43a4e6495478b2
eya jery <jaryeya2000@gmail.com>
2023-02-27 12:17:43 +0100

    commit

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from github import Github
 import pandas as pd
+import numpy as np
 
-project_list=['apache/any23','apache/dubbo']
 token="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"
-def project_info():
-    df_projects=pd.DataFrame()
-    for projects in project_list:
-        g=Github(token)
-        repo=g.get_repo(projects)
-        df_projects=df_projects.append({'project_id':repo.id,
-                                        'name':repo.full_name })
-    df=df_projects.to_csv('projects.csv', sep =',')
-    newdf=pd.read_csv('projects.csv')
-    print(newdf)
+g=Github(token)
+current_user=get_user()
+
                     
 
 
